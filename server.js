cb3c34e5f52523b369bbfb5e88858bcf17a81a9e
Lauren McNerney <lmcnern@outlook.com>
2015-04-23 13:52:34 -0700

    server update

--- server.js
+++ server.js
@@ -5,15 +5,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/secret', function(req, res) {
-  res.send('keep it secret, keep it safe.')
-})
-
-app.use(function(req, res, next) {
-  res.status(404).sendFile(__dirname + '/public/404.html');
-})
+app.use(function(request, response, next) {
+  response.status(404).sendFile(__dirname + '/public/404.html');
+});
 
 app.listen(app.get('port'), function() {
-  console.log('this node app is running with localhost:' +app.get('port'));
-})
-
+  console.log('Node app is running at localhost:' + app.get('port'));
+});
